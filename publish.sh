e66fbce9489abddebbf5cc8fb1836e9db35cf951
Zacharias Luiten <mail@zachariasluiten.com>
2015-12-29 10:51:36 +0100

    new

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+# make the script fail for any failed command
+set -e
+# make the script display the commands it runs to help debugging failures
+set -x
+
+# Go to the output directory
+cd output_prod
+
+# Remove the existing repo if it exists
+if [ -d ".git" ]; then
+    rm -rf .git
+fi
+
+# Create a repo for the built website for the master branch
+git init
+
+# configure env (locally)
+git config user.email 'github@zachariasluiten.com'
+git config user.name 'netiul publish script'
+
+# commit build
+git add .
+git commit -m "Build website"
+
+# push to GitHub Pages
+git push "https://github.com/netiul/netiul.github.com" -f master
